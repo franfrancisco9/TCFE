431b1bf0096507033b1a7af061351839ce0bd6f3
mcarvalho2001 <manuelmariafc@gmail.com>
2021-06-07 20:48:18 +0100

    fartooooo

--- T5/sim/Optimizing/merit.m
+++ T5/sim/Optimizing/merit.m
@@ -12,7 +12,7 @@ R3b = 100e3;
 R3c = 100e3;
 R4 = 1e3;
 
-system ("ngspice T5.cir");
+system ("ngspice /home/mcarvalho2001/Desktop/TCFE/T5/sim/Optimizing/T5.cir");
 cost_opamp = (8.661e-12 + 30e-12)*1000000 + (100000 + 5305 + 5305 + 1836 + 1836 + 13190000 + 50 + 100 + 18160)/1000 + (2*0.1);
 cost = cost_opamp + (R1 + R2 + R3 + R3b + R3c + R4)/1000 + (C1 + C2 + C3)*1000000;
 
